11382ca58e5e44bed0249f96a6217c9af343de88
devsince2021 <dev.ryu2021@gmail.com>
2021-04-25 14:03:48 +0900

    [f]: init

---
+++ travelparking/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? 'gray' : 'yellowgreen',
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View>
+        <Text>index</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
