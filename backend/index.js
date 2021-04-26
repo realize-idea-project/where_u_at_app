be543bd8fa8f950757c69bc80d828ad4eec786e4
devsince2021 <dev.ryu2021@gmail.com>
2021-04-27 00:53:51 +0900

    [b]: socket server

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,27 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 3001;
+const port = 3000;
+
+// io.on('connection', socket => {
+//   console.log('A user connected :D');
+//   socket.on('chat message', message => {
+//     console.log(message);
+//     io.emit('chat message', message);
+//     console.log('i sent message to client')
+//   });
+// });
 
 io.on('connection', socket => {
   console.log('A user connected :D');
-  socket.on('chat message', message => {
-    console.log(message);
-    io.emit('chat message', message);
-    console.log('i sent message to client')
-  });
-});
+
+  socket.on('send-location', (geolocation) => {
+    console.log('geolocation', geolocation);
+    socket.emit('receive-location', geolocation);
+    console.log('delevered');
+    // io.emit('receive-location', geolocation);
+    // console.log('delevered2');
+  })
+})
 
 server.listen(port, () => console.log('server is running on port: ' + port));
