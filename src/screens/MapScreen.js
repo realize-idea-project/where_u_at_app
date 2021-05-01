d067753bf728b5b8469706d585c32476217c5ef7
devsince2021 <dev.ryu2021@gmail.com>
2021-05-01 22:16:38 +0900

    connect to deployed server

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput, Alert } from "react-native";
 import Blank from '../components/Blank';
 import BackButton from '../components/Button/BackButton';
 import MapView from '../components/Map';
@@ -25,7 +25,8 @@ const MapScreen = ({ onPressBackButton, userInput }) => {
   const [memberLocations, setMemberLocations] = useState([]); // {name, location}
   
   useEffect(() => {
-    const newSocket = io(`http://192.168.0.246:3000`, { query: userInput });
+    // const newSocket = io(`http://192.168.0.246:8080`, { query: userInput });
+    const newSocket = io(`http://whereuat-env.eba-qzqmst7s.ap-northeast-2.elasticbeanstalk.com/`, { query: userInput });
     setSocket(newSocket);
     return () => newSocket.close();
   }, []);
@@ -33,12 +34,31 @@ const MapScreen = ({ onPressBackButton, userInput }) => {
   useEffect(() => {
     if (!socket) return;
     Geolocation.watchPosition(({ coords }) => {
+      console.log('watchPosition')
       const location = getCurrentLocation(coords, userInput);
       socket.emit('send-location', location);
-    }, { enableHighAccuracy: true, distanceFilter: 500 });
+    }, { enableHighAccuracy: true, distanceFilter: 400 });
+
+    Geolocation.getCurrentPosition(({ coords }) => {
+      console.log('getPosition')
+      const location = getCurrentLocation(coords, userInput);
+      socket.emit('send-location', location);
+    });
 
     socket.on('receive-location', (result)=> {
       console.log('result', result)
+      Alert.alert(
+        "신동 생축이요 ㅋㅋ",
+        `lat: ${result.location.latitude}, long: ${result.location.longitude}`,
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ]
+      );
       setMemberLocations([...memberLocations, result])
     });
 
